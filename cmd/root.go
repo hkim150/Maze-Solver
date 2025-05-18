7b411e9a1906e4e4fc0fe1c3d6ad1ebcf99c35a6
Hyun Bin Kim <hkim0150@gmail.com>
2025-05-17 17:02:13 -0700

    add maze generation algorithm flag in root command

--- cmd/root.go
+++ cmd/root.go
@@ -11,16 +11,33 @@ import (
 func init() {
 	rootCmd.Flags().IntP("width", "w", 20, "Width of the maze")
 	rootCmd.Flags().IntP("height", "l", 20, "Height of the maze")
+	rootCmd.Flags().StringP("algorithm", "a", "wilson", "Algorithm to use for maze generation (wilson, dfs, kruskal, prim)")
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "gen-and-solve",
+	// Use:   "gen-and-solve",
 	Short: "Generate and solve the maze",
 	Run: func(cmd *cobra.Command, args []string) {
 		width, _ := cmd.Flags().GetInt("width")
 		height, _ := cmd.Flags().GetInt("height")
+		algorithm, _ := cmd.Flags().GetString("algorithm")
+		var maze generator.Maze
+		var err error
+		switch algorithm {
+		case "dfs":
+			maze, err = generator.RandomizedDFS(width, height)
+		case "kruskal":
+			maze, err = generator.RandomizedKruskal(width, height)
+		case "prim":
+			maze, err = generator.RandomizedPrim(width, height)
+		case "wilson":
+			maze, err = generator.WilsonsAlgorithm(width, height)
+		default:
+			fmt.Println("Unknown algorithm:", algorithm)
+			fmt.Println("Available algorithms: dfs, kruskal, prim, wilson")
+			return
+		}
 
-		maze, err := generator.WilsonsAlgorithm(width, height)
 		if err != nil {
 			fmt.Println("Error generating maze:", err)
 			return
