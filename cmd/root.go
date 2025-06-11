1f6e3a7ff2d5cd1b5d7922a7aa77c4c44d63a7c0
Hyun Bin Kim <hkim0150@gmail.com>
2025-06-10 19:06:36 -0700

    add solver algorithm flag and clean up

--- cmd/root.go
+++ cmd/root.go
@@ -13,8 +13,9 @@ import (
 func init() {
 	rootCmd.Flags().IntP("width", "w", 25, "Width of the maze")
 	rootCmd.Flags().IntP("height", "l", 25, "Height of the maze")
-	rootCmd.Flags().StringP("algorithm", "a", "dfs", "Algorithm to use for maze generation (wilson, dfs, kruskal, prim, aldous-broder, recursive-division, fractal, eller, hunt-and-kill, sidewinder, binary-tree)")
-	rootCmd.Flags().BoolP("animate", "s", false, "Show generation animation")
+	rootCmd.Flags().StringP("generator", "g", "dfs", "maze generator algorithm - choose between [aldous-broder, binary-tree, dfs, eller, fractal-tessellation, hunt-and-kill, kruskal, prim, recursive-division, sidewinder, wilson]")
+	rootCmd.Flags().StringP("solver", "s", "dfs", "maze solver algorithm - choose between [a-star, bfs, dead-end-filling, dfs, hand-on-wall, lee, pledge, random-mouse, recursive, tremaux]")
+	rootCmd.Flags().BoolP("animate", "a", false, "Show generating and solving animation")
 }
 
 var rootCmd = &cobra.Command{
@@ -22,29 +23,26 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		width, _ := cmd.Flags().GetInt("width")
 		height, _ := cmd.Flags().GetInt("height")
-		algorithm, _ := cmd.Flags().GetString("algorithm")
+		genAlgo, _ := cmd.Flags().GetString("generator")
+		solverAlgo, _ := cmd.Flags().GetString("solver")
 		animate, _ := cmd.Flags().GetBool("animate")
 
-		m, err := generator.Generate(width, height, "dfs", false)
-		// m, err := generator.Generate(width, height, algorithm, animate)
+		m, err := generator.Generate(width, height, genAlgo, animate)
 		if err != nil {
 			fmt.Println("Error generating maze: ", err)
 			return
 		}
 
-		if animate {
-			m.PrintForAnimation(2 * time.Second)
-		}
-
-		err = solver.Solve(m, algorithm, animate)
+		// pause for a moment after showing the generated maze
+		m.PrintForAnimation(1500 * time.Millisecond)
+		
+		err = solver.Solve(m, solverAlgo, animate)
 		if err != nil {
 			fmt.Println("Error solving maze: ", err)
 			return
 		}
 
-		if animate {
-			m.PrintForAnimation(0 * time.Second)
-		}
+		m.PrintForAnimation(0)
 	},
 }
 
